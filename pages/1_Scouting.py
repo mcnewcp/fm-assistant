d520c32ceb52d8c78c0b6fd17bf306ff682ad141
Coy McNew <mcnewcp@gmail.com>
2024-01-06 14:33:04 -0600

    add statefullness to scouting page

--- pages/1_Scouting.py
+++ pages/1_Scouting.py
@@ -12,40 +12,47 @@ all_roles = df_role["Role"].unique()
 # sidebar
 with st.sidebar:
     # role selection
-    roles_scout = st.multiselect(
-        "Select roles for scoring scouted players",
+    roles = st.multiselect(
+        "Select roles for scoring squad",
         all_roles,
-        "DLR-Inverted Wing Back Su",
+        "DLR-Inverted Wing Back Su"
+        if "roles_scout" not in st.session_state
+        else st.session_state["roles_scout"],
         placeholder="Choose one or more roles",
     )
 
     # display columns selection
-    selectable_cols_scout = ["Age", "Personality", "Height", "Left Foot", "Right Foot"]
-    selected_cols_scout = st.multiselect(
+    selectable_cols = ["Age", "Personality", "Height", "Left Foot", "Right Foot"]
+    selected_cols = st.multiselect(
         "Select additional display columns",
-        selectable_cols_scout,
-        ["Age", "Personality"],
+        selectable_cols,
+        ["Age", "Personality"]
+        if "selected_cols_scout" not in st.session_state
+        else st.session_state["selected_cols_scout"],
         placeholder="Choose display columns",
     )
 
-    # scout file upload
-    uploaded_file_scout = st.file_uploader("Choose an HTML file", type="html")
+    # scouting file upload
+    uploaded_file = st.file_uploader("Choose an exported squad HTML", type="html")
 
-# load scout df
-if uploaded_file_scout is not None:
+# update session state
+st.session_state["roles_scout"] = roles
+st.session_state["selected_cols_scout"] = selected_cols
+
+# load players df
+if uploaded_file is not None:
     # Check if the uploaded file has an HTML extension
-    if not uploaded_file_scout.name.endswith(".html"):
+    if not uploaded_file.name.endswith(".html"):
         st.error("Error: Please upload a valid HTML file.")
     else:
         # Read HTML file
-        df_scout = read_html_file(uploaded_file_scout)
+        st.session_state["df_players_scout"] = read_html_file(uploaded_file)
 
-        if len(roles_scout) > 0:
-            # generate scored df
-            df_scout_scores, primary_attributes, secondary_attributes = score_players(
-                roles_scout, df_scout, selected_cols_scout
-            )
+if len(roles) > 0 and "df_players_scout" in st.session_state:
+    # generate scored df
+    df_scores, primary_attributes, secondary_attributes = score_players(
+        roles, st.session_state["df_players_scout"], selected_cols
+    )
 
-            if df_scout_scores is not None:
-                # Display the DataFrame in an interactive table
-                st.dataframe(df_scout_scores, use_container_width=True)
+    # display table
+    st.dataframe(df_scores, use_container_width=True)
